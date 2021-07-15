d3233e59d9e548800d3206d1e4af9fc61e3c5111
Maciej Kowalski <kowalski.exe@gmail.com>
2021-07-15 19:26:28 +0200

    typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import firebase from "firebase/app";
 import React from 'react';
+import firebase from "firebase/app";
+import 'firebase/performance';
+import 'firebase/analytics';
 import './css/App.css';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Header from './components/Header';
@@ -20,6 +22,8 @@ function App() {
     if (firebase.apps.length === 0) {
         firebase.initializeApp(firebaseConfig);
     }
+    const pref = firebase.performance();
+    const analytics = firebase.analytics();
 
     return (
         <Router>
@@ -46,7 +50,7 @@ function App() {
                         <h3>Oferta</h3>
                         <ul>
                             <li>Strona internetowa od zera</li>
-                            <li>Strona interteowan na bazie starej strony</li>
+                            <li>Strona internetowa na bazie starej strony</li>
                             <li>Strony oparte o CMS np. Wordpress lub Joomla</li>
                             <li>Strony responsywne</li>
                             <li>Grafika komputerowa</li>
@@ -57,23 +61,29 @@ function App() {
                             <div className='img-wrapper'>
                                 <img src='img/screenshots/lo1plan.png' alt='Plan lekcji dla LO im. Stanisława Staszica w Lublinie' />
                             </div>
-                            <img src='https://lo1plan.azurewebsites.net/img/staszic-beztekstu.png' className='favicon' alt='favicon strony' />
-                            <p className='link-p'>Plan lekcji dla LO im. Stanisława Staszica w Lublinie (lo1plan.azurewebsites.net)</p>
+                            <div className='link-wrapper'>
+                                <img src='https://lo1plan.azurewebsites.net/img/staszic-beztekstu.png' className='favicon' alt='favicon strony' />
+                                <p className='link-p'>Plan lekcji dla LO im. Stanisława Staszica w Lublinie (lo1plan.azurewebsites.net)</p>
+                            </div>
                         </a>
                         <hr />
                         <a rel='external noopener noreferrer' href='https://kule-problem-1.web.app/' target='_blank'>
                             <div className='img-wrapper'>
                                 <img src='img/screenshots/kule-problem-1.png' alt='Materiały do kółka informatycznego' />
                             </div>
-                            <p className='link-p'>Materiały do kółka informatycznego w I LO (kule-problem-1.web.app)</p>
+                            <div className='link-wrapper'>
+                                <p className='link-p'>Materiały do kółka informatycznego w I LO (kule-problem-1.web.app)</p>
+                            </div>
                         </a>
                         <hr />
                         <a rel='external noopener noreferrer' href='https://weekly-todo-list.web.app/' target='_blank' title="Progresywna aplikacja sieciowa 'Weekly ToDo'">
                             <div className='img-wrapper'>
                                 <img src='img/screenshots/weekly-todo.png' alt='Progresywna aplikacja sieciowa weekly todo' />
                             </div>
-                            <img src='https://weekly-todo-list.web.app/icons/icon144px.png' className='favicon' alt='favicon strony' />
-                            <p className='link-p'>Progresywna aplikacja sieciowa 'Weekly ToDo' (weekly-todo-list.web.app)</p>
+                            <div className='link-wrapper'>
+                                <img src='https://weekly-todo-list.web.app/icons/icon144px.png' className='favicon' alt='favicon strony' />
+                                <p className='link-p'>Progresywna aplikacja sieciowa 'Weekly ToDo' (weekly-todo-list.web.app)</p>
+                            </div>
                         </a>
                         <hr />
                         <img src='https://github.githubassets.com/favicons/favicon.svg' className='favicon-smaller' alt='github icon' />
